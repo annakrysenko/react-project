7e391a713a337e95c9b4d1ad2ea949eb2d6ff56a
Ihor Kalyta <ihor.kalyta93@gmail.com>
2023-03-13 19:19:56 +0100

    1.3

--- src/pages/Training.jsx
+++ src/pages/Training.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { useState, useEffect, useCallback } from 'react';
+// import React from 'react';
+// import { useState, useEffect, useCallback } from 'react';
 
-import myGoal from 'components/MyGoal/MyGoal';
+// import myGoal from 'components/MyGoal/MyGoal';
 // import Training from 'components/MyTraining';
 
 
-const Training = () => {
-  return <div>Training</div>;
-};
+// const Training = () => {
+//   return <div>Training</div>;
+// };
 
-export default Training;
+// export default Training;
