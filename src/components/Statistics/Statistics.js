f1539e8b1aa4fa80f2ea13bb2d1733da17a977e2
flappykled <dima04089804@gmail.com>
2023-03-15 14:29:04 +0200

    test

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,61 @@
+// import styled from "styled-components";
+
+// export const ModalWrapper = styled.div`
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   right: 0;
+//   bottom: 0;
+//   background-color: rgba(0, 0, 0, 0.5);
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+// export const ModalContent = styled.div`
+//   background-color: #fff;
+//   padding: 1rem;
+//   border-radius: 0.5rem;
+//   box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.2);
+// `;
+
+// export const ModalHeader = styled.div`
+//   font-size: 1.5rem;
+//   font-weight: bold;
+//   margin-bottom: 1rem;
+// `;
+
+// export const ModalBody = styled.div`
+//   font-size: 1.2rem;
+//   line-height: 1.5;
+//   margin-bottom: 1rem;
+// `;
+
+// export const ModalFooter = styled.div`
+//   display: flex;
+//   justify-content: flex-end;
+//   margin-top: 1rem;
+
+//   & > *:not(:first-child) {
+//     margin-left: 1rem;
+//   }
+// `;
+
+// export const ModalButton = styled.button`
+//   font-size: 1rem;
+//   padding: 0.5rem 1rem;
+//   border: none;
+//   border-radius: 0.25rem;
+//   background-color: #0077cc;
+//   color: #fff;
+//   cursor: pointer;
+
+//   &:hover {
+//     background-color: #005fa3;
+//   }
+
+//   &:disabled {
+//     opacity: 0.5;
+//     cursor: not-allowed;
+//   }
+// `;
