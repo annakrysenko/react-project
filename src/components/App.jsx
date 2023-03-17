a73afe57ba87dd054b8de1e8115747a24ddb7fd4
Anna Krysenko <krysenkoanna@gmail.com>
2023-03-17 02:57:38 +0200

    add sid and refresh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import Login from 'pages/Login';
 import MobileAddBook from 'pages/MobileAddBook';
 import Register from 'pages/Register';
 import Training from 'pages/Training';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, Route, Routes, useSearchParams } from 'react-router-dom';
 // import { fetchCurrentUser } from 'redux/auth/authOperation';
 // import { getfetchCurrentUser } from 'redux/auth/authSelectors';
@@ -13,22 +13,26 @@ import Layout from './Layout/Layout';
 import PrivateRoute from './PrivateRoute/PrivateRoute';
 import PublicRoute from './PublicRoute/PublicRoute';
 import { addAccessToken } from 'redux/auth/authSlice';
-import { token } from 'redux/auth/authOperation';
+import { fetchCurrentUser, token } from 'redux/auth/authOperation';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { getfetchCurrentUser } from 'redux/auth/authSelectors';
 
 export const App = () => {
   const dispatch = useDispatch();
-  // const isFetching = useSelector(getfetchCurrentUser);
-  // const [firstRenderEnded, setFirstRenderEnded] = useState(false);
+  const isFetching = useSelector(getfetchCurrentUser);
+  const [firstRenderEnded, setFirstRenderEnded] = useState(false);
 
-  // useEffect(() => {
-  //   dispatch(fetchCurrentUser());
-  //   setFirstRenderEnded(true);
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchCurrentUser());
+    setFirstRenderEnded(true);
+    console.log('isFetching', isFetching);
+  }, [dispatch, isFetching]);
 
   const [searchParams] = useSearchParams();
-  const accessToken = searchParams.get('accessToken');
+  const accessToken = searchParams.get('token');
+  console.log('accessToken params', accessToken);
+
   useEffect(() => {
     if (accessToken !== null) {
       dispatch(addAccessToken(accessToken));
@@ -37,67 +41,67 @@ export const App = () => {
   }, [accessToken, dispatch]);
 
   return (
-    // !isFetching &&
-    // firstRenderEnded && (
-    <>
-      {/* <ToastContainer position="top-right" autoClose={2000} theme="colored" /> */}
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route
-            index
-            element={
-              <PublicRoute restricted>
-                <Login />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="register"
-            element={
-              <PublicRoute restricted>
-                <Register />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="login"
-            element={
-              <PublicRoute restricted>
-                <Login />
-              </PublicRoute>
-            }
-          />
+    !isFetching &&
+    firstRenderEnded && (
+      <>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={
+                <PublicRoute restricted>
+                  <Login />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="register"
+              element={
+                <PublicRoute restricted>
+                  <Register />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="login"
+              element={
+                <PublicRoute restricted>
+                  <Login />
+                </PublicRoute>
+              }
+            />
 
-          <Route
-            path="library"
-            element={
-              <PrivateRoute>
-                <Library />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="library/addBook"
-            element={
-              <PrivateRoute>
-                <MobileAddBook />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="training"
-            element={
-              <PrivateRoute>
-                <Training />
-              </PrivateRoute>
-            }
-          />
-        </Route>
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-      <ToastContainer position="top-right" autoClose={2000} theme="colored" />
-    </>
+            <Route
+              path="library"
+              element={
+                <PrivateRoute>
+                  <Library />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="library/addBook"
+              element={
+                <PrivateRoute>
+                  <MobileAddBook />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="training"
+              element={
+                <PrivateRoute>
+                  <Training />
+                </PrivateRoute>
+              }
+            />
+          </Route>
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+        <ToastContainer position="top-right" autoClose={2000} theme="colored" />
+      </>
+    )
   );
   // );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -16,7 +16,7 @@ import {
 const authPersistConfig = {
   key: 'auth',
   storage,
-  whitelist: ['token', 'user'],
+  whitelist: ['token', 'user', 'refreshToken', 'sid'],
 };
 
 export const store = configureStore({
