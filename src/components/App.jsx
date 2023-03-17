91746ac370611ac4a93aa6cf4eb0d9d3aeb06fd2
Anna Krysenko <107724360+annakrysenko@users.noreply.github.com>
2023-03-17 13:02:27 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -85,7 +85,7 @@ export const App = () => {
               element={
                 <PrivateRoute>
                   <MobileAddBook />
-                </MobileRoute>
+                
               </PrivateRoute>
             }
           />
