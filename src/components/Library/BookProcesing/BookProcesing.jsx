493598f123e1a7f5de47dbca90b52fe08037466d
VasylTehlivets <vasyltehlivets@gmail.com>
2023-03-18 23:53:29 +0200

    init bookCh

--- src/components/Library/BookProcesing/BookProcesing.jsx
+++ src/components/Library/BookProcesing/BookProcesing.jsx
@@ -1,114 +1,3 @@
-// import LibraryForm from '../LibraryForm/LibraryForm';
-// import MobileTable from '../MobileBooks/MobileBooks';
-// import Table from './Table/Table';
-// import ReadTable from './ReadTable/ReadTable';
-// // import Container from 'components/Container';
-// // import {
-// //   MobileAddBtn,
-// //   StyledButton,
-// //   StyledSection,
-// //   TextStyled,
-// //   TextStyledPrimary,
-// // } from './LibraryComponent.styled';
-// import useLibraryComponent from './useLibraryComponent';
-// import { ReactComponent as PlusIcon } from '../BooksIcon/plus.svg';
-// import EmtpyLibraryText from 'components/Modal/EmtpyLibraryText';
-
-// const LibraryComponent = () => {
-//   const {
-//     isMobile,
-//     alreadyBooks,
-//     nowBooks,
-//     planBooks,
-//     navigate,
-//     isEmpty,
-//     isLoading,
-//     error,
-//   } = useLibraryComponent();
-
-//   return (
-//     <>
-//       <div>
-//         {error && <p>{error?.data?.message}</p>}
-//         {!isLoading && (
-//           <section>
-//             {isMobile ? (
-//               <>
-//                 {!isEmpty && (
-//                   <>
-//                     <p>Library empty</p>
-//                     <span>Add</span>
-//                   </>
-//                 )}
-
-//                 {!!alreadyBooks.length && (
-//                   <MobileTable
-//                     title={'Already read'}
-//                     status={false}
-//                     data={alreadyBooks}
-//                   />
-//                 )}
-//                 {!!nowBooks.length && (
-//                   <MobileTable title={'reading now'} status={true} data={nowBooks} />
-//                 )}
-//                 {!!planBooks.length && (
-//                   <MobileTable
-//                     title={'Going to read'}
-//                     status={false}
-//                     data={planBooks}
-//                   />
-//                 )}
-//                 <MobileAddBtn
-//                   type="primary"
-//                   onClick={() => {
-//                     navigate('/library/addBook');
-//                   }}
-//                 >
-//                   <PlusIcon />
-//                 </MobileAddBtn>
-//               </>
-//             ) : (
-//               <>
-//                 <LibraryForm />
-//                 {!!alreadyBooks.length && (
-//                   <ReadTable
-//                     title={'Already read'}
-//                     status={false}
-//                     data={alreadyBooks}
-//                   />
-//                 )}
-//                 <EmtpyLibraryText isEmptyLibrary={isEmpty} />
-//                 {!!nowBooks.length && (
-//                   <Table title={'reading now'} status={true} data={nowBooks} />
-//                 )}
-//                 {!!planBooks.length && (
-//                   <Table
-//                     title={'Going to read'}
-//                     status={false}
-//                     data={planBooks}
-//                   />
-//                 )}
-//               </>
-//             )}
-
-//             {isEmpty && (
-//               <button
-//                 type="primary"
-//                 onClick={() => {
-//                   navigate('/training');
-//                 }}
-//               >
-//                 Моє тренування
-//               </button>
-//             )}
-//           </section>
-//         )}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default LibraryComponent;
 
 import { useSelector } from 'react-redux';
 import {
@@ -144,17 +33,17 @@ function LibraryFilld(props) {
 	const finishedReading = useSelector(getFinishedReading);
     const currentlyReading = useSelector(getCurrentlyReading);
     
-     const toggleModal = (id) => {
-    // ваш код для відкриття/закриття модального вікна
-         console.log(toggleModal);
-  };
+//      const toggleModal = (id) => {
+//     // ваш код для відкриття/закриття модального вікна
+//          console.log(toggleModal);
+//   };
 
 	return (
 		<>
 			<LibraryControlTabletSection>
 				{finishedReading.length > 0 && (
 					<LibraryBooklistContainer tabletSize>
-						<LibraryBooklistTitle tabletSize>Прочитав</LibraryBooklistTitle>
+						<LibraryBooklistTitle tabletSize>Already read</LibraryBooklistTitle>
 						<LibraryBooklistTabletTable>
 							<LibraryBooklistTabletHead>
 								<tr>
@@ -369,7 +258,7 @@ function LibraryFilld(props) {
 										</LibraryBooklistTable>
 										<LibraryBooklistButton
 											type="button"
-											onClick={() => toggleModal(_id)}
+											onClick={() => props.toggleModal(_id)}
 										>
 											Резюме
 										</LibraryBooklistButton>
@@ -464,8 +353,6 @@ function LibraryFilld(props) {
 					</LibraryBooklistContainer>
 				)}
 			</LibraryControlMobileSection>
-
-			{/* <ResumeModal /> */}
 		</>
 	);
 }
